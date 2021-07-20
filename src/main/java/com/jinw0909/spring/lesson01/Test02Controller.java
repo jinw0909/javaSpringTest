db781efe01357229d685b427a71e94ede8c91dda
Jinwoo Chang <>
2021-07-20 12:37:55 +0900

    [장진우] - java spring quiz commit / added new RestController for quiz#2

---
+++ src/main/java/com/jinw0909/spring/lesson01/Test02Controller.java
@@ -0,0 +1,12 @@
+package com.jinw0909.spring.lesson01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/lesson01/test02")
+@RestController
+public class Test02Controller {
+	
+	@RequestMapping("/2")
+	 
+}
