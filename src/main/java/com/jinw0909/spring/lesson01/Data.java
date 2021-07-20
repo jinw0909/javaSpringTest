df730f13165bb68dc9c3325490fbfc5065ab7309
Jinwoo Chang <>
2021-07-20 11:40:31 +0900

    [장진우] - 스프링 예제 풀이

---
+++ src/main/java/com/jinw0909/spring/lesson01/Data.java
@@ -0,0 +1,18 @@
+package com.jinw0909.spring.lesson01;
+
+public class Data {
+	private String subject;
+	private int score;
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
